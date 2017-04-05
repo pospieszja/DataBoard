5fbfe9329682c3ba5e74b0be2d1572d72539d8d9
pospieszja <j.pospieszynski@outlook.com>
2017-04-05 23:07:45 +0200

    add UserDto

---
+++ src/DataBoard.Infrastructure/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DataBoard.Infrastructure.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+    }
+}
